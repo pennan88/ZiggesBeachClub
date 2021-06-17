1b443c5b7d8384aeca1bc1cc43dfb7f72aad52c5
Alexander Fagrelius <erikalexander98@hotmail.com>
2021-06-17 18:01:13 +0200

    Fixade med färger och har gjort api anropp till instagram

---
+++ JS/FeedArray.js
@@ -0,0 +1,19 @@
+let FeedOne = {
+  Description: document.querySelector("#CaptionOne"),
+  background: document.querySelector(".FeedOne"),
+  feedurl: document.querySelector("#FeedUrlOne"),
+};
+
+let FeedTwo = {
+  Description: document.querySelector("#CaptionTwo"),
+  background: document.querySelector(".FeedTwo"),
+  feedurl: document.querySelector("#FeedUrlTwo"),
+};
+
+let FeedThree = {
+  Description: document.querySelector("#CaptionThree"),
+  background: document.querySelector(".FeedThree"),
+  feedurl: document.querySelector("#FeedUrlThree"),
+};
+
+let FeedArray = [FeedOne, FeedTwo, FeedThree];
---
+++ JS/api.js
@@ -0,0 +1,31 @@
+"use strict";
+const short_tok =
+  "IGQVJVdFBBZA2xTOVFfVm0xMjIzOTY2UXhDNXZAQNGRwNGpXd0h3N0Nmamlna2tTLXl3UXNYeGQ5SGVLNzZAUWjdVVTNtUm9WT2RpYkJTNFZAMcGk0cjkxbVpVYjlRYi1FdzVwRi0tNXQ1cDlnRjFnQUZACTwZDZD";
+const url = `https://v1.nocodeapi.com/pennan88/instagram/SwxgfVAgzdHEOWNd`;
+
+const test = document.querySelector("#instafeedtest");
+
+async function InstaAPI() {
+  const url = `https://v1.nocodeapi.com/pennan88/instagram/SwxgfVAgzdHEOWNd`;
+
+  const response = await fetch(url);
+  let urlData = await response.json();
+
+  return urlData;
+}
+
+for (let i = 0; i < 3; i++) {
+  let RandomNum = Math.floor(Math.random() * 8);
+  InstaAPI().then(function ApiData(urlData) {
+    console.log(urlData);
+    FeedArray[i].Description.innerHTML = urlData.data[RandomNum].caption;
+    FeedArray[i].background.style.background = `url(${
+      urlData.data[RandomNum].thumbnail_url ?? urlData.data[RandomNum].media_url
+    })`;
+    FeedArray[i].background.style.backgroundPosition = "center";
+    FeedArray[i].background.style.backgroundSize = "cover";
+    FeedArray[i].background.style.backgroundRepeat = "no-repeat";
+    FeedArray[i].feedurl.href = urlData.data[RandomNum].permalink;
+    console.log(RandomNum);
+  });
+}
