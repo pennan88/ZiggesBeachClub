f12a5df62cb76e6d29f156bce71bd07eb610bfa1
Alexander Fagrelius <erikalexander98@hotmail.com>
2021-06-17 11:25:16 +0200

    First

---
+++ Javascript/api.js
@@ -0,0 +1,5 @@
+var feed = new Instafeed({
+    accessToken: 'IGQVJVdFBBZA2xTOVFfVm0xMjIzOTY2UXhDNXZAQNGRwNGpXd0h3N0Nmamlna2tTLXl3UXNYeGQ5SGVLNzZAUWjdVVTNtUm9WT2RpYkJTNFZAMcGk0cjkxbVpVYjlRYi1FdzVwRi0tNXQ1cDlnRjFnQUZACTwZDZD',
+    limit: 1
+  });
+  feed.run();
